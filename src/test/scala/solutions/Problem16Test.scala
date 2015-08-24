e777d7b35af94c71577466c2583e323fbf72e2da
Mrunmayee Sabnis <Mrunmayee.Sabnis@crest.in>
2015-08-24 21:54:26 +0530

    Mrunmayee Added problem16 solution and the test

---
+++ src/test/scala/solutions/Problem16Test.scala
@@ -0,0 +1,9 @@
+package solutions
+
+import org.scalatest.{Matchers, FunSpec}
+import solutions.Problem16
+
+class Problem16Test extends FunSpec with Matchers{
+  val problem16 = new Problem16
+  problem16.
+}
