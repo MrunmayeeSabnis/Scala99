01d8f06efef8b3dfafc5869718484b060c837969
Mrunmayee Sabnis <Mrunmayee.Sabnis@crest.in>
2015-08-24 21:27:38 +0530

    Mrunmayee Added tests for problem17

---
+++ src/main/scala/solutions/Problem17.scala
@@ -0,0 +1,14 @@
+package solutions
+
+class Problem17{
+
+  def split[A](index: Int, list: List[A]): (List[A], List[A]) = (index, list) match{
+    case (index, _) if index < 0 => throw new ArrayIndexOutOfBoundsException
+    case (index, Nil) => (Nil, Nil)
+    case (0, head :: tail) => (Nil, head :: tail)
+    case (index, head :: tail) =>  {
+      val (pre, post) = split(index - 1, tail)
+      (head :: pre, post)
+    }
+  }
+}
---
+++ src/test/scala/solutions/Problem17Test.scala
@@ -0,0 +1,12 @@
+package solutions
+
+import org.scalatest.{Matchers, FunSpec}
+
+class Problem17Test extends FunSpec with Matchers {
+  describe("Problem17"){
+    it("should return splitted list"){
+      val problem16 = new Problem17
+      problem16.split(-1, List(6, 1, 9, 2, 8, 3)) should be((List(6, 1), List(9, 2, 8, 3)))
+    }
+  }
+}
